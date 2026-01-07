932eea71223689a270510f30069c7e5b7aa99690
Deveshree101 <deveshreeee@gmail.com>
2026-01-07 13:37:33 +0000

    Deploying to gh-pages from @ Deveshree101/Deveshree101.github.io@7476a49b23243b2bba2b2804d5c1c9395aa5227e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,8 +419,8 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-predicting-high-risk-borrowers",
+          title: 'Predicting High Risk Borrowers',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
